1f0c0c76ad0b045f15d15ec19ab0bb55cfd6d9ff
blackparadise0407 <blackparadise0407@gmail.com>
2020-12-09 20:20:24 +0700

    fix socket issue

--- src/utils/websocket.js
+++ src/utils/websocket.js
@@ -1,14 +1,15 @@
+var users = [];
 class WebSockets {
   // users = []
   // constructor() {
   //   this.users = [];
   // }
   connection(client) {
-    let users = [];
     console.log('New user connected');
     client.on('disconnect', () => {
-      users = users.filter((user) => user.socketId !== client.id);
-      console.log('User disconected');
+      users = [...users.filter((user) => user.socketId !== client.id)];
+      client.emit('updateUsers', []);
+      console.log('User disconected', users);
     });
     client.on('identity', ({ id: userId, username }) => {
       users.push({
