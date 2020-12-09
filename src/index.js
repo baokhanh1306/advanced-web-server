2872086abec926f28bde7de7fe52e30df1337e58
Khanh Bảo <baokhanh1306nsc@gmail.com>
2020-12-09 20:25:15 +0700

    Fix cors

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ app.use((err, req, res, next) => {
 const server = http.createServer(app);
 const io = socketIO(server, {
   cors: {
-    origin: *,
+    origin: '*',
   }
 });
 io.on('connection', websocket.connection);
