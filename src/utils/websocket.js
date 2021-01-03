148f1d407deaf5f2858efd7abe2ac36b7b6e81eb
Khanh Bảo <baokhanh1306nsc@gmail.com>
2021-01-03 19:06:36 +0700

    Add password

--- src/utils/websocket.js
+++ src/utils/websocket.js
@@ -7,23 +7,23 @@ let boards = [];
 
 const genHist = () => {
   return _.map(Array(BOARD_SIZE).fill(null), () => {
-    return Array(BOARD_SIZE).fill(null)
-  })
-}
+    return Array(BOARD_SIZE).fill(null);
+  });
+};
 
 const BOARD_SIZE = 20;
 
 (async function () {
-  boards = await Board.find({})
+  boards = await Board.find({});
 
-  boards = _.map(boards, b => {
+  boards = _.map(boards, (b) => {
     return {
       grid: JSON.parse(JSON.stringify(genHist())),
       ...b.toObject()
-    }
-  })
+    };
+  });
   console.log(boards);
-})()
+})();
 
 let users = [];
 
@@ -39,7 +39,8 @@ module.exports = function (io, socket) {
       socketId: socket.id,
       userId,
       username,
-      _id
+      _id,
+      grid: JSON.parse(JSON.stringify(genHist()))
     });
     socket.user = userId;
     socket.username = username;
@@ -47,51 +48,49 @@ module.exports = function (io, socket) {
   });
   socket.on('create-board', async ({ name, user, password = '' }) => {
     // const id = uuidv4();
-    const newBoard = await Board.create({ name, playerX: user });
+    const newBoard = await Board.create({ name, playerX: user, password });
     boards.push({
       _id: newBoard._id,
       name: newBoard.name,
       playerX: user,
-      password,
-      history: Array(20).fill(null).map(() => Array(20).fill(null)),
+      password
     });
     socket.board = newBoard._id;
     socket.join(newBoard._id);
-    socket.emit('board-id', newBoard._id);
+    io.to(socket.board).emit('user-join-room', { newBoard, user, size });
   });
-  socket.on('join-board', ({ boardId, user }) => {
+  socket.on('join-board', ({ boardId, user, password }) => {
     let size = 0;
-    console.log(boardId, boards)
-    const board = _.find(boards, b => b._id.toString() === boardId)
-    console.log(board);
-    console.log('USER JOIN', user);
-    if (board) {
-      const { playerX, playerO } = board
-      if (playerX) {
-        console.log('if1');
-        board.playerO = user;
-      }
-      else if (playerO) {
-        console.log('if2');
+    const board = _.find(boards, (b) => b._id.toString() === boardId);
+    if (password === board.password || board.password === '') {
+      console.log(board);
+      console.log('USER JOIN', user);
+      if (board) {
+        const { playerX, playerO } = board;
+        if (playerX) {
+          console.log('if1');
+          board.playerO = user;
+        } else if (playerO) {
+          console.log('if2');
 
-        board.playerX = user;
-      }
-      if (!playerX && !playerO) {
-        console.log('if3');
+          board.playerX = user;
+        }
+        if (!playerX && !playerO) {
+          console.log('if3');
 
-        board.playerX = user;
-      }
-      if (board.playerX || board.playerO) size = 1;
-      if (board.playerX && board.playerO) size = 2;
+          board.playerX = user;
+        }
+        if (board.playerX || board.playerO) size = 1;
+        if (board.playerX && board.playerO) size = 2;
 
-      socket.board = boardId;
-      socket.join(boardId);
-      console.log('cc', socket.board);
-      io.to(socket.board).emit('user-join-room', { board, user, size });
+        socket.board = boardId;
+        socket.join(boardId);
+        io.to(socket.board).emit('user-join-room', { board, user, size });
+      }
     }
   });
   socket.on('leave-board', async ({ boardId, user }) => {
-    const board = _.find(boards, b => b._id.toString() === boardId)
+    const board = _.find(boards, (b) => b._id.toString() === boardId);
     if (board) {
       const { playerX, playerO } = board;
       if (playerX && user === playerX.toString()) {
@@ -113,11 +112,23 @@ module.exports = function (io, socket) {
   });
   socket.on('play-at', ({ row, col, val }) => {
     console.log(chalk('play-at'), { row, col, val });
-    const board = _.find(boards, b => b._id.toString() === socket.board.toString());
+    const board = _.find(
+      boards,
+      (b) => b._id.toString() === socket.board.toString()
+    );
     board.grid[row][col] = val;
     if (checkWin(row, col, val, board.grid)) {
       io.to(socket.board).emit('win', { msg: 'Ok win' });
     }
     io.to(socket.board).emit('move', { row, col, val });
+  });
+  socket.on('join-room-id', ({ id, password }) => {
+    const board = boards[id];
+    if (password === board.password || board.password === '') {
+      socket.board = board._id;
+      socket.join(board._id);
+
+      io.to(socket.board).emit('user-join-room-id', board);
+    }
   });
 };
