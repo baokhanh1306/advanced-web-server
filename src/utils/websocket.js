7026c391e3ea04e4d45350cf96eb5ede9b825432
blackparadise0407 <blackparadise0407@gmail.com>
2021-01-12 21:10:30 +0700

    add surrender, draw

--- src/utils/websocket.js
+++ src/utils/websocket.js
@@ -3,6 +3,7 @@ const _ = require('lodash');
 const checkWin = require('./checkwin');
 const Board = require('../board/board.model');
 const chalk = require('chalk');
+const { forEach } = require('lodash');
 
 
 const genHist = () => {
@@ -55,6 +56,7 @@ module.exports = function (io, socket) {
       },
       history: [],
       currentUsers: [user],
+      isXTurn: true,
       moveDuration
     };
     boards.push(board);
@@ -65,9 +67,12 @@ module.exports = function (io, socket) {
   });
   socket.on('join-board', ({ boardId, user }) => {
     const board = _.find(boards, (b) => b._id.toString() === boardId);
+    // const otherBoard = _.filter(boards, b=>b._id !==boardId)
+    // _.map(otherBoard, b=> )
     if (board) {
       const { playerX, playerO } = board;
-      board.currentUsers.push(user)
+      if (!_.includes(board.currentUsers, user)) board.currentUsers.push(user)
+
 
       if (playerX && playerO) {
         socket.board = boardId;
@@ -88,38 +93,39 @@ module.exports = function (io, socket) {
       io.emit('list-board', { data: boards });
     }
   });
-  socket.on('leave-board', async ({ boardId, user }) => {
+  socket.on('surrender', async ({ boardId, user }) => {
     const board = _.find(boards, (b) => b._id.toString() === boardId);
     if (board) {
-      board.currentUsers = _.filter(board.currentUsers, per => per !== user)
-      if ((board.playerX && user === board.playerX.toString()) || (board.playerO && user === board.playerO.toString())) {
+      // board.currentUsers = _.filter(board.currentUsers, per => per !== user)
+      // if ((board.playerX && user === board.playerX.toString()) || (board.playerO && user === board.playerO.toString())) {
 
-        if (!board.isReady.X || !board.isReady.O) {
-          const { playerX, playerO } = board;
-          if (playerX && user === playerX.toString()) {
-            board.playerX = null;
-          }
-          if (playerO && user === playerO.toString()) {
-            board.playerO = null;
-          }
-          // if (board.playerX || board.playerO) board.status = '1/2 players';
-          // if (!board.playerX && !board.playerO) board.status = '0/2 players';
-          io.to(boardId).emit('user-leave-room', { msg: `User ${user} has left`, playingUsers });
-          io.emit('list-board', { data: boards });
-        }
-        else {
-          board.winner = user === board.playerX.toString() ? 1 : -1
-          let val = board.winner === 1 ? 'O' : 'X'
-          console.log(board);
-          io.to(socket.board).emit('win', { winner: val, grid: board.grid, board });
-          boards = _.filter(boards, b => b._id !== board._id)
-          io.emit('list-board', ({ data: boards }))
-          // _id: uuid, grid, isReady, status, ...rest
-          const { isReady, status, _id, grid, ...rest } = board
-          const newBoard = new Board(rest)
-          await newBoard.save()
-        }
-      }
+      //   if (!board.isReady.X || !board.isReady.O) {
+      //     const { playerX, playerO } = board;
+      //     if (playerX && user === playerX.toString()) {
+      //       board.playerX = null;
+      //     }
+      //     if (playerO && user === playerO.toString()) {
+      //       board.playerO = null;
+      //     }
+      //     // if (board.playerX || board.playerO) board.status = '1/2 players';
+      //     // if (!board.playerX && !board.playerO) board.status = '0/2 players';
+      //     io.to(boardId).emit('user-leave-room', { msg: `User ${user} has left`, playingUsers });
+      //     io.emit('list-board', { data: boards });
+      //   }
+      // else {
+      board.winner = user === board.playerX.toString() ? 1 : -1
+      let val = board.winner === 1 ? 'O' : 'X'
+      let loser = board.winner === 1 ? 'X' : 'O'
+
+      io.to(socket.board).emit('surrender', { winner: val, loser, grid: board.grid, board });
+      boards = _.filter(boards, b => b._id !== board._id)
+      io.emit('list-board', ({ data: boards }))
+      // _id: uuid, grid, isReady, status, ...rest
+      const { isReady, status, _id, grid, isXTurn, ...rest } = board
+      const newBoard = new Board(rest)
+      await newBoard.save()
+      // }
+      // }
     }
   });
   socket.on('send-message', ({ username, msg }) => {
@@ -140,12 +146,12 @@ module.exports = function (io, socket) {
     });
   });
   socket.on('play-at', async ({ row, col, val }) => {
-    console.log(chalk('play-at'), { row, col, val });
     const board = _.find(
       boards,
       (b) => b._id.toString() === socket.board.toString()
     );
     board.grid[row][col] = val;
+    board.isXTurn = !board.isXTurn
     board.history.push(JSON.stringify(board.grid));
     if (checkWin(row, col, val, board.grid)) {
       console.log("win");
@@ -155,11 +161,11 @@ module.exports = function (io, socket) {
       boards = _.filter(boards, b => b._id !== board._id)
       io.emit('list-board', ({ data: boards }))
       // _id: uuid, grid, isReady, status, ...rest
-      const { isReady, _id, grid, currentUsers, moveDuration, ...rest } = board
+      const { isReady, _id, grid, currentUsers, moveDuration, isXTurn, ...rest } = board
       const newBoard = new Board(rest)
       await newBoard.save()
     }
-    io.to(socket.board).emit('move', { row, col, val });
+    io.to(socket.board).emit('move', { row, col, val, isXTurn: board.isXTurn });
   });
   socket.on('search-room-id', ({ roomId, user }) => {
     const board = boards[parseInt(roomId)];
@@ -174,7 +180,7 @@ module.exports = function (io, socket) {
       if (!playerX && !playerO) {
         board.playerX = user;
       }
-      board.currentUsers.push(user)
+      if (!_.includes(board.currentUsers, user)) board.currentUsers.push(user)
       socket.board = board._id;
       socket.join(board._id);
       io.to(socket.board).emit('user-join-room', { board, playingUsers });
@@ -202,7 +208,7 @@ module.exports = function (io, socket) {
   socket.on('play-now', async ({ userId, cups }) => {
     let found = false;
     for (user of playNowUsers) {
-      if (Math.abs(user.cups - cups) <= 5 && user.userId !== userId) {
+      if (Math.abs(user.cups - cups) <= 1000 && user.userId !== userId) {
         const id = uuidv4();
         const board = {
           _id: id,
@@ -218,7 +224,8 @@ module.exports = function (io, socket) {
             O: null
           },
           history: [],
-          moveDuration: 20
+          moveDuration: 20,
+          isXTurn: true
         };
         boards.push(board);
         socket.board = board._id;
@@ -256,5 +263,24 @@ module.exports = function (io, socket) {
     playNowUsers = _.filter(playNowUsers, (u) => u.userId !== userId);
     console.log(playNowUsers);
     socket.emit(`cancel-ok-${userId}`)
+  })
+
+  socket.on('request-draw', ({ boardId, userId }) => {
+    const board = _.find(boards, b => b._id === boardId)
+    const { playerX, playerO } = board
+    const otherUser = userId === playerX ? playerO : playerX
+    io.emit(`request-draw-${otherUser}`)
+  })
+
+  socket.on('accept-draw', ({ boardId }) => {
+    boards = _.filter(boards, b => b._id !== boardId)
+    io.emit('list-board', ({ data: boards }))
+    io.to(boardId).emit('accept-draw')
+  })
+  socket.on('cancel-draw', ({ boardId, userId }) => {
+    const board = _.find(boards, b => b._id === boardId)
+    const { playerX, playerO } = board
+    const otherUser = userId === playerX ? playerO : playerX
+    io.emit(`cancel-draw-${otherUser}`)
   })
 };
